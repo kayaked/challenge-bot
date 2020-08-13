6a2ac238b7009bb60b3ef5addc1c3778361ecb01
Jack <31581463+kayaked@users.noreply.github.com>
2020-08-13 16:31:58 -0400

    Add files via upload

---
+++ api.py
@@ -0,0 +1,17 @@
+from quart import Quart
+import json
+from motor import motor_asyncio
+client = motor_asyncio.AsyncIOMotorClient()
+db = client['Discord']
+
+app = Quart(__name__)
+
+@app.route('/list')
+async def clist():
+    challenge_list = await db.levels.find({'placement': {'$lte': 50}}).to_list(length=50)
+    return challenge_list
+
+@app.route('/legacy')
+async def llist():
+    legacy_list = await db.levels.find({'placement': {'$gt': 50}}).to_list(length=50)
+    return legacy_list
