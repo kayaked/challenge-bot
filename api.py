1ce40f2a598b53fa53af1870fbd1e105d53c12d5
Jack <31581463+kayaked@users.noreply.github.com>
2020-08-13 17:05:57 -0400

    Update api.py

--- api.py
+++ api.py
@@ -16,4 +16,4 @@ async def llist():
     legacy_list = await db.levels.find({'placement': {'$gt': 50}}).to_list(length=50)
     return legacy_list
 
-app.run(debug=True)
+app.run(debug=True, port=80)
