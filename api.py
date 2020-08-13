1c83ef238c6ee3af4b7f5df337725161ff916bfb
Jack <31581463+kayaked@users.noreply.github.com>
2020-08-13 18:05:05 -0400

    Update api.py

--- api.py
+++ api.py
@@ -8,12 +8,16 @@ db = client['Discord']
 @route('/list')
 def clist():
     challenge_list = list(db.levels.find({'placement': {'$lte': 50}}))
+    for lvl in challenge_list:
+        lvl['_id'] = str(lvl['_id'])
     response.content_type = 'application/json'
     return json.dumps(challenge_list)
 
 @route('/legacy')
 def llist():
     legacy_list = list(db.levels.find({'placement': {'$gt': 50}}))
+    for lvl in legacy_list:
+        lvl['_id'] = str(lvl['_id'])
     response.content_type = 'application/json'
     return json.dumps(legacy_list)
 
