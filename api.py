5be226f29534430ed8c2cd1457951533e1550e33
Jack <31581463+kayaked@users.noreply.github.com>
2020-08-14 12:22:17 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,19 @@
-from bottle import route, run, response
+from bottle import route, run, response, Bottle
 import json
 from pymongo import MongoClient
 
 client = MongoClient()
 db = client['Discord']
 
-@route('/list')
+app = Bottle()
+
+@app.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+
+@app.route('/list')
 def clist():
     challenge_list = list(db.levels.find({'placement': {'$lte': 50}}))
     for lvl in challenge_list:
@@ -13,7 +21,7 @@ def clist():
     response.content_type = 'application/json'
     return json.dumps(challenge_list)
 
-@route('/legacy')
+@app.route('/legacy')
 def llist():
     legacy_list = list(db.levels.find({'placement': {'$gt': 50}}))
     for lvl in legacy_list:
@@ -21,4 +29,4 @@ def llist():
     response.content_type = 'application/json'
     return json.dumps(legacy_list)
 
-run(host='0.0.0.0', port=443, debug=True, server='gunicorn', keyfile='key.pem', certfile='cert.pem')
+app.run(host='0.0.0.0', port=443, debug=True, server='gunicorn', keyfile='key.pem', certfile='cert.pem')
