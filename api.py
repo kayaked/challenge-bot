3411745b22af52ebfb560cfed3b18065db7c3e41
Jack <31581463+kayaked@users.noreply.github.com>
2020-08-15 15:07:15 -0400

    Update api.py

--- api.py
+++ api.py
@@ -10,8 +10,8 @@ app = Bottle()
 @app.hook('after_request')
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 @app.route('/list')
 def clist():
@@ -30,5 +30,16 @@ def llist():
     legacy_list = sorted(legacy_list, key=lambda i: i['placement'])
     response.content_type = 'application/json'
     return json.dumps(legacy_list)
+
+@app.route('/level/<placement>')
+def level():
+    try:
+        pl = int(placement)
+    except:
+        return json.dumps({'error': 'dumbfuck'})
+    level = db.levels.find_one({'placement': pl})
+    records = list(db.records.find({'challenge': level['name']}))
+    level['victors'] = [record for record in records if record['player'] != level['verifier']]
+    return json.dumps(level)
 
 app.run(host='0.0.0.0', port=443, debug=True, server='gunicorn', keyfile='key.pem', certfile='cert.pem')
